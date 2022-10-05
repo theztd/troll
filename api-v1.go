3c2dd8df670b357f779c2bd0a54e8a4bf326ea86
Marek Sirovy <msirovy@gmail.com>
2022-10-05 10:40:00 +0200

    Add headers debuging endpoint

--- api-v1.go
+++ api-v1.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"net/http/httputil"
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/requestid"
@@ -48,6 +50,14 @@ func v1Info(c *gin.Context) {
 	})
 }
 
+func v1AllHeaders(c *gin.Context) {
+	reqDump, _ := httputil.DumpRequest(c.Request, true)
+	fmt.Println(string(reqDump))
+	c.JSON(http.StatusOK, gin.H{
+		"rec_headers": strings.Split(string(reqDump), "\r\n"),
+	})
+}
+
 func v1RoutesAdd(rtG *gin.RouterGroup) {
 	r := rtG.Group("/")
 	log.Println("Loading V1 routes...")
@@ -56,6 +66,7 @@ func v1RoutesAdd(rtG *gin.RouterGroup) {
 
 	r.GET("/status", v1Status)
 	r.GET("/info", v1Info)
+	r.GET("/headers", v1AllHeaders)
 	r.GET("/:item/*id", slowResponse)
 	r.POST("/:item/*id", slowResponse)
 }
